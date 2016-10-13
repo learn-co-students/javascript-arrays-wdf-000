8d04d6eb894739cd040abbba507cb36075eb941f
ozPop <ozziepopovas@gmail.com>
2016-10-13 17:08:04 -0400

    Done.

---
+++ arrays.js
@@ -0,0 +1,39 @@
+var chocolateBars = [
+  'snickers', 'hundred grand', 'kitkat', 'skittles'
+];
+
+function addElementToBeginningOfArray(array, element) {
+  var arr = [...array];
+  arr.unshift(element);
+  return arr;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  var arr = [...array];
+  arr.push(element);
+  return arr;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+
+function removeElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
