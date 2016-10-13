09985f0b1bb088dc8277e6d312167ec2f9fa3cd2
Flatiron School <ellis.paige.v@gmail.com>
2016-10-13 16:06:46 -0400

    Done.

---
+++ arrays.js
@@ -0,0 +1,43 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, elem) {
+  var a = [elem,...array]
+  return a
+}
+
+function destructivelyAddElementToBeginningOfArray(array, elem) {
+  array.unshift(elem)
+  return array
+}
+
+function addElementToEndOfArray(array, elem) {
+  var a = [...array, elem]
+  return a
+}
+
+function destructivelyAddElementToEndOfArray(array, elem) {
+  array.push(elem)
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
+}
